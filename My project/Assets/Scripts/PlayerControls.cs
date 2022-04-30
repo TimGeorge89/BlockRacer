72bcc50ad94c30ba763066b0d7569665081dafe6
Tim <Fallenpyr@aol.com>
2022-04-29 20:08:26 -0400

    Setting Up Player Movement
    Player Control script created

--- My project/Assets/Scripts/PlayerControls.cs
+++ My project/Assets/Scripts/PlayerControls.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 500f;
+    
+
     // Start is called before the first frame update
-    void Start()
+    void Start() 
     {
         
     }
 
+    //Using FixedUpdate for messing with physics
     // Update is called once per frame
-    void Update()
+    void FixedUpdate() 
     {
-        
+        // Apply forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("a")) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+
+        } else if (Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
